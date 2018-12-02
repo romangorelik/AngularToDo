c10d34b8b0ccbc61bd2cc0ece9b6f85004eb6043
Roman Gorelik <romang31@gmail.com>
2018-12-02 13:03:20 -0500

    Finished ToDo application.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'AngularToDo';
+  todos: any[] = [{task: 'task1', complete: false, update: ''}]
+  todo: string;
+  backColor: string;
+
+  addTodo(): void {
+    this.todos.push({task: this.todo, complete: false, update: ''})
+    this.todo = ''
+  }
+
+  deleteTodo(ind: number): void {
+    this.todos.splice(ind, 1)
+  } 
+
+  completeTodo(ind: number): void {
+    this.todos[ind].complete = !this.todos[ind].complete
+    setTimeout(() => {
+      this.deleteTodo(ind)
+    }, 4000)
+  }
+
+  update(ind: number, str: string): void {
+    this.todos[ind].update = str
+    this.todos[ind].task = this.todos[ind].update
+    this.todos[ind].update = ''
+  }
 }
